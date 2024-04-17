00295f4e19437cdacec698e747d2872d972dd0d9
AnnyBianchi <150509539+AnnyBianchi@users.noreply.github.com>
2024-04-17 11:07:54 +0200

    Update daticonnessione.php
    modificato i dati della connessione al database con quelli forniti dalla prof.

--- pagine/daticonnessione.php
+++ pagine/daticonnessione.php
@@ -1,9 +1,9 @@
 <?php
     //variabili utili alla connessione
     $HOSTDB="localhost";
-    $USERDB="root";
-    $PASSDB="root";
-    $NAMEDB="protezione_civile_db"; 
+    $USERDB="user-5am-2324";
+    $PASSDB="A4tE7qN88a-ahBs";
+    $NAMEDB="db-5am-2324"; 
 
 
 ?>
